d656e0b39a353b1413b9bd1adfc9f0b9d05f5107
Thomas Dippel <thomas@dippel.dk>
2020-10-03 13:42:54 +0200

    implement logic for puzzle 1

---
+++ Assets/Scripts/Puzzles/Puzzle1/PressurePlateController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Puzzles
+{
+    namespace Puzzle1
+    {
+        public class PressurePlateController : MonoBehaviour
+        {
+            private void OnTriggerEnter(Collider other)
+            {
+                EventManager.Instance.OnPressurePlateEnter(other.gameObject);
+            }
+
+            private void OnTriggerExit(Collider other)
+            {
+                EventManager.Instance.OnPressurePlateExit(other.gameObject);
+            }
+        }
+    }
+}
