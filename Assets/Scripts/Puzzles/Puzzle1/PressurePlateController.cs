988c2f4d856f32445dd466918a1bce41fefd494c
Thomas Dippel <thomas@dippel.dk>
2020-10-03 22:28:48 +0200

    don't trigger pressure plates for CharacterController

--- Assets/Scripts/Puzzles/Puzzle1/PressurePlateController.cs
+++ Assets/Scripts/Puzzles/Puzzle1/PressurePlateController.cs
@@ -8,11 +8,17 @@ namespace Puzzles
         {
             private void OnTriggerEnter(Collider other)
             {
+                if (other.GetType().Equals(typeof(CharacterController)))
+                    return;
+
                 EventManager.Instance.OnPressurePlateEnter(other.gameObject);
             }
 
             private void OnTriggerExit(Collider other)
             {
+                if (other.GetType().Equals(typeof(CharacterController)))
+                    return;
+
                 EventManager.Instance.OnPressurePlateExit(other.gameObject);
             }
         }
--- Assets/Scripts/Puzzles/Puzzle4/PressurePlateController.cs
+++ Assets/Scripts/Puzzles/Puzzle4/PressurePlateController.cs
@@ -8,11 +8,17 @@ namespace Puzzles
         {
             private void OnTriggerEnter(Collider other)
             {
+                if (other.GetType().Equals(typeof(CharacterController)))
+                    return;
+
                 PuzzleManager.Instance.OnPressurePlateEnter(this.gameObject, other.gameObject);
             }
 
             private void OnTriggerExit(Collider other)
             {
+                if (other.GetType().Equals(typeof(CharacterController)))
+                    return;
+
                 PuzzleManager.Instance.OnPressurePlateExit(this.gameObject, other.gameObject);
             }
         }
