63531ef299e40294d8c4b6f0487ff30f65614a0b
PierreNS <pierreskinnerup@gmail.com>
2020-10-03 08:54:53 +0200

    Packages
    Added Cinemachine and started working on PlayerMove.

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float playerSpeed;
+
+
+    private CharacterController characterController;
+    private Vector3 playerVelocity;
+    private Vector3 moveDir;
+    private bool groundedPlayer;
+    private float gravityValue = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();    
+        HandleMove();    
+    }
+
+    void HandleInput() 
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveDir = new Vector3(x, 0, z).normalized;
+    }
+    void HandleMove() 
+    {
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        characterController.Move(moveDir * Time.deltaTime * playerSpeed);
+    }
+}
