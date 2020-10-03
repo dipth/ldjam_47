3b1904f9057bd1532dfaf2a561b2c420efe4538d
PierreNS <pierreskinnerup@gmail.com>
2020-10-03 17:01:12 +0200

    Commiting

--- Assets/Scripts/Player/GhostManager.cs
+++ Assets/Scripts/Player/GhostManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class GhostManager : MonoBehaviour
 {
     public static GhostManager instance;
+    List<Ghost> ghosts = new List<Ghost>();
+
+    public Ghost ghostPrefab;
+    public Transform ghostParent;
+    TimeController timeController;
 
     private void Awake()
     {
@@ -12,5 +17,30 @@ public class GhostManager : MonoBehaviour
             instance = this;
         else
             Destroy(this);
+
+        timeController = GameObject.FindGameObjectWithTag("Player").GetComponent<TimeController>();
+        timeController.OnTimeRewind += CreateGhost;
+    }
+
+    public void AddGhostToList(Ghost ghost) 
+    {
+        ghosts.Add(ghost);
+    }
+
+    public void GhostBusters() 
+    {
+        for (int i = 0; i < ghosts.Count; i++)
+        {
+            Destroy(ghosts[i].gameObject);
+        }
+        ghosts.Clear();
+    }
+
+    public void CreateGhost()
+    {
+        Ghost clone;
+        clone = Instantiate(ghostPrefab, transform.position, Quaternion.identity, ghostParent);
+        clone.AddPath(timeController.points);
+        GhostManager.instance.AddGhostToList(clone);
     }
 }
